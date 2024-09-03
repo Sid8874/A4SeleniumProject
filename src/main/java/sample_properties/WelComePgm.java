116daeeb8ab4b2ffdd788a3c4b5d9bb5659e0ba3
user <user@DESKTOP-9M5IC3D>
2024-09-03 14:26:45 +0530

    changes 1

--- src/main/java/sample_properties/WelComePgm.java
+++ src/main/java/sample_properties/WelComePgm.java
@@ -1,5 +1,13 @@
 package sample_properties;
 
+import org.testng.annotations.Test;
+
 public class WelComePgm {
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("WEL Come To Belagavi");
+	}
 
 }
